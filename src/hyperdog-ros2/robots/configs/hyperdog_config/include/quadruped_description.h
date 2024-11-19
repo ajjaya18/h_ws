588f14cac680a98f5b2da07b2c017dd9f420e03e
ajjaya18 <ajjaya@espol.edu.ec>
2024-11-19 11:02:01 -0500

    changes 16.3

--- src/hyperdog-ros2/robots/configs/hyperdog_config/include/quadruped_description.h
+++ src/hyperdog-ros2/robots/configs/hyperdog_config/include/quadruped_description.h
@@ -9,26 +9,25 @@ namespace champ
     {
         void loadFromHeader(champ::QuadrupedBase &base)
         {
-            base.lf.hip.setOrigin(0.175, 0.105, 0, 0, 0, 0);
-            base.lf.upper_leg.setOrigin(0, 0.06, 0, 0, 0, 0);
-            base.lf.lower_leg.setOrigin(0, 0, -0.141, 0, 0, 0);
-            base.lf.foot.setOrigin(0, 0, -0.141, 0, 0, 0);
+            base.lf.hip.setOrigin(0.175, 0.066, 0.0, 0.0, 0.0, 0.0);
+            base.lf.upper_leg.setOrigin(0.0, 0.104, -0.0, 0.0, 0.0, 0.0);
+            base.lf.lower_leg.setOrigin(-0.15, 0.0, -0.0, 0.0, 0.0, 0.0);
+            base.lf.foot.setOrigin(0.14, 0.0, -0.0, 0.0, 0.0, 0.0);
 
-            base.rf.hip.setOrigin(0.175, -0.105, 0, 0, 0, 0);
-            base.rf.upper_leg.setOrigin(0, -0.06, 0, 0, 0, 0);
-            base.rf.lower_leg.setOrigin(0, 0, -0.141, 0, 0, 0);
-            base.rf.foot.setOrigin(0, 0, -0.141, 0, 0, 0);
+            base.rf.hip.setOrigin(0.175, -0.066, 0.0, 0.0, 0.0, 0.0);
+            base.rf.upper_leg.setOrigin(0.0, -0.104, -0.0, 0.0, 0.0, 0.0);
+            base.rf.lower_leg.setOrigin(-0.15, 0.0, -0.0, 0.0, 0.0, 0.0);
+            base.rf.foot.setOrigin(0.14, 0.0, -0.0, 0.0, 0.0, 0.0);
 
-            base.lh.hip.setOrigin(-0.175, 0.105, 0, 0, 0, 0);
-            base.lh.upper_leg.setOrigin(0, 0.06, 0, 0, 0, 0);
-            base.lh.lower_leg.setOrigin(0, 0, -0.141, 0, 0, 0);
-            base.lh.foot.setOrigin(0, 0, -0.141, 0, 0, 0);
+            base.lh.hip.setOrigin(-0.175, 0.066, 0.0, 0.0, 0.0, 0.0);
+            base.lh.upper_leg.setOrigin(0.0, 0.104, -0.0, 0.0, 0.0, 0.0);
+            base.lh.lower_leg.setOrigin(-0.15, 0.0, -0.0, 0.0, 0.0, 0.0);
+            base.lh.foot.setOrigin(0.14, 0.0, -0.0, 0.0, 0.0, 0.0);
 
-            base.rh.hip.setOrigin(-0.175, -0.105, 0, 0, 0, 0);
-            base.rh.upper_leg.setOrigin(0, -0.06, 0, 0, 0, 0);
-            base.rh.lower_leg.setOrigin(0, 0, -0.141, 0, 0, 0);
-            base.rh.foot.setOrigin(0, 0, -0.141, 0, 0, 0);
-        }
+            base.rh.hip.setOrigin(-0.175, -0.066, 0.0, 0.0, 0.0, 0.0);
+            base.rh.upper_leg.setOrigin(0.0, -0.104, -0.0, 0.0, 0.0, 0.0);
+            base.rh.lower_leg.setOrigin(-0.15, 0.0, -0.0, 0.0, 0.0, 0.0);
+            base.rh.foot.setOrigin(0.14, 0.0, -0.0, 0.0, 0.0, 0.0);
     }
 }
 #endif
