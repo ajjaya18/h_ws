1b2250deec5122496c06f235a1131c48e3c2c129
ajjaya18 <ajjaya@espol.edu.ec>
2024-11-20 11:05:19 -0500

    changes 16.4

--- src/hyperdog-ros2/robots/configs/hyperdog_config/include/gait_config.h
+++ src/hyperdog-ros2/robots/configs/hyperdog_config/include/gait_config.h
@@ -4,13 +4,13 @@
 #define KNEE_ORIENTATION        ">>"
 #define ODOM_SCALER             1.0
 #define PANTOGRAPH_LEG          false
-#define MAX_LINEAR_VELOCITY_X   0.5
-#define MAX_LINEAR_VELOCITY_Y   0.25
-#define MAX_ANGULAR_VELOCITY_Z  1.0
+#define MAX_LINEAR_VELOCITY_X   0.3
+#define MAX_LINEAR_VELOCITY_Y   0.2
+#define MAX_ANGULAR_VELOCITY_Z  0.5
 #define COM_X_TRANSLATION       0.0
-#define SWING_HEIGHT            0.04
+#define SWING_HEIGHT            0.03
 #define STANCE_DEPTH            0.00
-#define STANCE_DURATION         0.25
-#define NOMINAL_HEIGHT          0.20
+#define STANCE_DURATION         0.35
+#define NOMINAL_HEIGHT          0.18
 
 #endif
